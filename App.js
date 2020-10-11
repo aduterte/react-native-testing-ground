43b20b2423d0167eb8ce6756fed7424d672bfc26
aduterte <alexduterte@gmail.com>
2020-10-10 22:16:29 -0400

    testing nested components

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from "./Components/Test.js"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <StatusBar style="auto" />
+        <Text>Test2</Text>
+      </View>
+      <View style={styles.container}>
+        <Test/>
+      </View>
+      
+
     </View>
   );
 }
