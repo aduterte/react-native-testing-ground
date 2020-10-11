4d1d6709eea559ac83a303e3c7db2fd2b6d1601c
aduterte <alexduterte@gmail.com>
2020-10-11 15:37:20 -0400

    image test

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import Test from "./Components/Test.js"
 
 export default function App() {
@@ -11,9 +11,12 @@ export default function App() {
         <StatusBar style="auto" />
         <Text>Learning React-Native should be fun!</Text>
       </View>
-      
+      <Image
+        style={styles.tinyLogo}
+        source={{uri: 'https://www.pngitem.com/pimgs/m/148-1487614_spotify-logo-small-spotify-logo-transparent-hd-png.png'}}
+      />
         <Test/>
-      
+       
       
 
     </View>
@@ -26,5 +29,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
